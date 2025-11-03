cf209ff4a8631014576553237b780de306dcd8dd
0allinone0 <seongjini726@gmail.com>
2025-11-02 21:42:47 -0600

    1st finalized

--- js/script.js
+++ js/script.js
@@ -11,3 +11,22 @@ async function get_giphy(){
 }
 
 get_giphy()
+
+// a. store the div container
+const gif_container = document.querySelector("#gif-container");
+
+// b.
+const fetch_button = document.querySelector("#fetch-gif-btn");
+
+// c.
+fetch_button.addEventListener("click", async function() {
+    gif_container.innerHTML = "";
+
+    //call fetch_gif to get images array
+    const images = await get_giphy();
+
+    //d. loop through the array and display each image
+    for (let i=0;i<images.length; i++){
+        gif_container.innerHTML += `<img src=${images[i]} class = "col-3 mb-3">`;
+    }
+});
