3df8919b354f5d4a47645087c9d749c8e476e839
0allinone0 <seongjini726@gmail.com>
2025-11-02 21:52:29 -0600

    final one(no additional credit)

--- js/script.js
+++ js/script.js
@@ -6,11 +6,13 @@ async function get_giphy(){
     const response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=H0CssqhPQI7Gvgl0Cnmz9EFff4ELZSRQ&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips");
     const data = await response.json();
     const images = data.data.map(gif => gif.images.original.url);
-    
+    return images;
+    /*
     console.log(images); //preview
+*/
 }
 
-get_giphy()
+//get_giphy()
 
 // a. store the div container
 const gif_container = document.querySelector("#gif-container");
@@ -26,7 +28,7 @@ fetch_button.addEventListener("click", async function() {
     const images = await get_giphy();
 
     //d. loop through the array and display each image
-    for (let i=0;i<images.length; i++){
+    for (let i=0; i<images.length; i++){
         gif_container.innerHTML += `<img src=${images[i]} class = "col-3 mb-3">`;
     }
 });
